45c39805da378bb757d2217033e71b8eb9780f0e
smin9408 <stmdaks888@naver.com>
2022-02-21 17:44:27 +0900

    Initial commit

---
+++ app/src/main/java/com/knta/baemin_clone/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.knta.baemin_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpEvents()
+        setUpValues()
+
+    }
+
+
+    fun setUpEvents(){
+
+    }
+
+    fun setUpValues(){
+
+    }
+}
