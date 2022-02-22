80d35419ed55c7db5475d4ceaef5ff12bf244f63
smin9408 <stmdaks888@naver.com>
2022-02-22 11:01:51 +0900

    getView 함수 기본 구조

--- app/src/main/java/com/knta/baemin_clone/adapters/StoreAdapter.kt
+++ app/src/main/java/com/knta/baemin_clone/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.knta.baemin_clone.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.knta.baemin_clone.R
 import com.knta.baemin_clone.datas.StoreData
 
 class StoreAdapter(
@@ -9,4 +13,17 @@ class StoreAdapter(
     resId: Int,
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
