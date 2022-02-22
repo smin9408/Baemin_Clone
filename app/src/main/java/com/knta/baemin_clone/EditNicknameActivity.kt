46d0181f986b6319e3e09d42dec72d59f3aff582
smin9408 <stmdaks888@naver.com>
2022-02-22 15:45:37 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/com/knta/baemin_clone/EditNicknameActivity.kt
@@ -0,0 +1,25 @@
+package com.knta.baemin_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+
+    }
+
+    fun setValues() {
+
+
+    }
+
+
+}
