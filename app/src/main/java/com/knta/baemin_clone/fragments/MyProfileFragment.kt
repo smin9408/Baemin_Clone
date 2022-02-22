8f3701dc7b446a4301c05c0cded946cfabdb3ecf
smin9408 <stmdaks888@naver.com>
2022-02-22 15:59:00 +0900

    프래그먼트로 돌아왔을 때 , 첨부한 닉네임 반영

--- app/src/main/java/com/knta/baemin_clone/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/knta/baemin_clone/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.knta.baemin_clone.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -46,5 +47,17 @@ class MyProfileFragment : Fragment() {
 
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_NICKNAME) {
+            if (resultCode == Activity.RESULT_OK) {
+                val nickname = data!!.getStringExtra("nick")
+                txtNickname.text = nickname
+            }
+        }
+    }
+
 
 }
