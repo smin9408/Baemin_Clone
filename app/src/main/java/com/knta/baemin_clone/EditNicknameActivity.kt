b6ca999057d52f6a1cbf4233d679dca9f84aa85d
smin9408 <stmdaks888@naver.com>
2022-02-22 15:53:48 +0900

    변경 화면으로 들어와서 > 저장하기 클릭시, 새 입력 닉네임 가지고 복귀

--- app/src/main/java/com/knta/baemin_clone/EditNicknameActivity.kt
+++ app/src/main/java/com/knta/baemin_clone/EditNicknameActivity.kt
@@ -1,7 +1,9 @@
 package com.knta.baemin_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,16 @@ class EditNicknameActivity : AppCompatActivity() {
 
     fun setupEvents() {
 
+        btnSave.setOnClickListener {
+
+            val inputNickname = edtNickname.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", inputNickname)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+
+
+        }
 
     }
 
