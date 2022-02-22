b79e5b1b2cbc9a649f261fcc6888f3a448656c34
smin9408 <stmdaks888@naver.com>
2022-02-22 15:20:45 +0900

    전화 주문 버튼 클릭 > 권한 물어보기만

--- app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
@@ -1,14 +1,18 @@
 package com.knta.baemin_clone
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.knta.baemin_clone.datas.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
-    lateinit var mStoreData : StoreData
+    lateinit var mStoreData: StoreData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +22,33 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setUpEvents(){
+    fun setUpEvents() {
+
+//        권한 관련 변수를, 화면이 만들어질 때 한 번만 세팅.
+        val pl = object : PermissionListener {
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@ViewStoreDetailActivity, "통화 권한이 없습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
+
+        btnCall.setOnClickListener {
+
+//            클릭될 때마다 만든 변수를 사용만 하는 걸로.
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
 
     }
 
-    fun setValues(){
+    fun setValues() {
 
         Glide.with(this).load(mStoreData.logoImageURL).into(imgLogo)
 
