36a7bad53f5b1b8a53dc788ef6172ca858399ab7
smin9408 <stmdaks888@naver.com>
2022-02-22 11:18:10 +0900

    ListView에 띄우기 성공

--- app/src/main/java/com/knta/baemin_clone/fragments/StoreListFragment.kt
+++ app/src/main/java/com/knta/baemin_clone/fragments/StoreListFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.knta.baemin_clone.R
+import com.knta.baemin_clone.adapters.StoreAdapter
 import com.knta.baemin_clone.datas.StoreData
+import kotlinx.android.synthetic.main.fragment_store_list.*
 
 class StoreListFragment : Fragment() {
 
     val mPizzaStoreList = ArrayList<StoreData>()
+    lateinit var mPizzaStoreAdapter : StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,6 +41,9 @@ class StoreListFragment : Fragment() {
         mPizzaStoreList.add( StoreData("파파존스", 3.5, "1577-8080","http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800" ))
         mPizzaStoreList.add( StoreData("도미노피자", 5.toDouble(), "1577-3082","https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png" ))
         mPizzaStoreList.add( StoreData("미스터피자", 4.0, "1577-0077","https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200" ))
+
+        mPizzaStoreAdapter = StoreAdapter(requireContext(), R.layout.store_list_item, mPizzaStoreList)
+        StoreListView.adapter = mPizzaStoreAdapter
     }
 
 }
