bd05ba7ddb2d20284fb96ccec6be221d40ebdace
smin9408 <stmdaks888@naver.com>
2022-02-22 14:46:50 +0900

    첨부해준 데이터 받아오기  - 멤버 변수로 만들어서 활용

--- app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
@@ -2,10 +2,26 @@ package com.knta.baemin_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.knta.baemin_clone.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+        setUpEvents()
+        setValues()
     }
+
+    fun setUpEvents(){
+
+    }
+
+    fun setValues(){
+
+    }
+
 }
