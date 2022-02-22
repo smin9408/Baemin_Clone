9d51730791bd2e2533845ff8583c266e16e4dd02
smin9408 <stmdaks888@naver.com>
2022-02-22 09:59:08 +0900

    getCount / getItem 오버라이딩 + 내용 구현

--- app/src/main/java/com/knta/baemin_clone/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/knta/baemin_clone/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,22 @@
 package com.knta.baemin_clone.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.knta.baemin_clone.fragments.MyProfileFragment
+import com.knta.baemin_clone.fragments.StoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount() = 2
+
+
+    override fun getItem(position: Int): Fragment {
+
+        return when (position) {
+            0 -> StoreListFragment()
+            else -> MyProfileFragment()
+        }
+    }
+
+
 }
