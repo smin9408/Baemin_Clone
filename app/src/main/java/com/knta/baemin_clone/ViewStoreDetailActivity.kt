ad90d6113c2b67158c0c97054911e7857df22cc5
smin9408 <stmdaks888@naver.com>
2022-02-22 15:12:38 +0900

    실제 데이터 표시하기

--- app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/knta/baemin_clone/ViewStoreDetailActivity.kt
@@ -2,7 +2,9 @@ package com.knta.baemin_clone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.knta.baemin_clone.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -22,6 +24,15 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
     fun setValues(){
 
+        Glide.with(this).load(mStoreData.logoImageURL).into(imgLogo)
+
+        txtStoreName.text = mStoreData.name
+
+        txtStoreNumber.text = mStoreData.phoneNum
+
+        ratingBar.rating = mStoreData.rating.toFloat()
+
+        txtRating.text = "(${mStoreData.rating})"
     }
 
 }
